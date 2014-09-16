c43312dd74778dddaeeb1e7d6d2029e609b8ab30
danielarusu <danielarusu88@gmail.com>
2014-09-16 14:07:13 +0300

    starting the node server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,20 @@
-console.log('Server started on port');
+var http = require('http');
+var express = require('express');
+var jwt = require('jwt-simple');
+var restful = require('node-restful');
+var path = require('path');
+var fs = require('fs');
+
+GLOBAL.app = express();
+
+var port = 8080;
+
+app.use('/', express.static(path.resolve('../client')));
+
+/*app.get('*', function(req, res){
+    res.sendfile('./client/index.html');
+})*/
+
+app.listen(port);
+
+console.log('Server started on port' + port);
