44162caf22e50084fa550b5df8c195564ca09746
danielarusu <danielarusu88@gmail.com>
2014-11-21 18:39:42 +0200

    login form

--- client/app/js/controllers/MainController.js
+++ client/app/js/controllers/MainController.js
@@ -1,7 +1,7 @@
-MASTERS_APP_MODULE.controller('MainController',
+MASTERS_APP_MODULE.controller('MainController',['$scope',
     function($scope){
-
         $scope.navPath = FOLDER_PATH + 'templates/views/partials/header.html';
-        $scope.footerPath = FOLDER_PATH + 'templates/views/partials/footer.html'
+        $scope.footerPath = FOLDER_PATH + 'templates/views/partials/footer.html';
+        $scope.homePath = FOLDER_PATH + 'templates/views/home.html';
     }
-);
+]);
